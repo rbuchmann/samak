b8251cf84c05585bba48243e4a5fb801672206f6
Rasmus Buchmann <rasmus.buchmann@freiheit.com>
2017-11-10 14:17:42 +0100

    Update spec parser

--- ui_src/samak/spec_parse.clj
+++ ui_src/samak/spec_parse.clj
@@ -8,18 +8,45 @@
                       (f conformed)
                       conformed)))))
 
+(defn qualify-kw
+  ([kw] (qualify-kw *ns* kw))
+  ([ns kw] (keyword (str ns) (name kw))))
+
+(defn to-spec-params [bindings]
+  (mapcat (fn [[k v]] [(keyword k) v]) (partition 2 bindings)))
+
+(defmacro defnodespec [name bindings body]
+  `(s/def ~(qualify-kw name)
+     (let [spec# (s/cat ~@(to-spec-params bindings))]
+       (s/conformer
+        (fn [val#]
+          (let [conformed# (s/conform spec# val#)
+                {:keys ~(mapv first (partition 2 bindings))} conformed#]
+            (if (= :clojure.spec.alpha/invalid conformed#)
+              :clojure.spec.alpha/invalid
+              (assoc ~body
+                     :samak.nodes/type ~(qualify-kw "samak.nodes" name)))))))))
+
+(defnodespec define
+  [lhs symbol?
+   _ #{'=}
+   rhs ::literal]
+  {:lhs lhs
+   :rhs rhs})
+
+
+
 (s/def ::map (s/map-of :keyword? ::simple-expression))
 
-(def literal-spec (s/or :int            integer?
-                        :float-literal  number?
-                        :bool-literal   boolean?
-                        :symbol-literal symbol?
-                        :map-literal    ::map))
+(s/def literal-spec (s/or :int            integer?
+                          :float-literal  number?
+                          :bool-literal   boolean?
+                          :symbol-literal symbol?
+                          :map-literal    ::map))
 
-(s/def ::literal (s/conformer
-                  (fn [val]
-                    (cond->> (s/conform literal-spec val)
-                        #(not= :clojure.spec.alpha/invalid %) (zipmap [:type :value])))))
+(defnodespec literal
+  [lit literal-spec]
+  (zipmap [:samak.nodes/type :samak.nodes/value] lit))
 
 
 (defn bin-op [sym]
